d6abfdea49d8ef67c37ac3f1b4a29877ba22e4b7
Ryukishi07 <68122209+Ryukishi07@users.noreply.github.com>
2020-09-05 21:02:43 +0300

    Add files via upload

---
+++ 2D_Animation/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace _2D_Animation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.рисоватьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.движениеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.пускToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.стопToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.рисоватьToolStripMenuItem,
+            this.движениеToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // рисоватьToolStripMenuItem
+            // 
+            this.рисоватьToolStripMenuItem.Name = "рисоватьToolStripMenuItem";
+            this.рисоватьToolStripMenuItem.Size = new System.Drawing.Size(84, 24);
+            this.рисоватьToolStripMenuItem.Text = "Рисовать";
+            this.рисоватьToolStripMenuItem.Click += new System.EventHandler(this.рисоватьToolStripMenuItem_Click);
+            // 
+            // движениеToolStripMenuItem
+            // 
+            this.движениеToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.пускToolStripMenuItem,
+            this.стопToolStripMenuItem});
+            this.движениеToolStripMenuItem.Name = "движениеToolStripMenuItem";
+            this.движениеToolStripMenuItem.Size = new System.Drawing.Size(97, 24);
+            this.движениеToolStripMenuItem.Text = "Движение ";
+            // 
+            // пускToolStripMenuItem
+            // 
+            this.пускToolStripMenuItem.Enabled = false;
+            this.пускToolStripMenuItem.Name = "пускToolStripMenuItem";
+            this.пускToolStripMenuItem.Size = new System.Drawing.Size(117, 26);
+            this.пускToolStripMenuItem.Text = "Пуск";
+            this.пускToolStripMenuItem.Click += new System.EventHandler(this.пускToolStripMenuItem_Click);
+            // 
+            // стопToolStripMenuItem
+            // 
+            this.стопToolStripMenuItem.Enabled = false;
+            this.стопToolStripMenuItem.Name = "стопToolStripMenuItem";
+            this.стопToolStripMenuItem.Size = new System.Drawing.Size(117, 26);
+            this.стопToolStripMenuItem.Text = "Стоп";
+            this.стопToolStripMenuItem.Click += new System.EventHandler(this.стопToolStripMenuItem_Click);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(65, 24);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(221, 183);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 396);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(196, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Угловая скорость (рад/сек.) ";
+            this.label1.Visible = false;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Enabled = false;
+            this.numericUpDown1.Location = new System.Drawing.Point(15, 416);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Visible = false;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Enabled = false;
+            this.numericUpDown2.Location = new System.Drawing.Point(15, 371);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown2.TabIndex = 5;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Visible = false;
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(12, 351);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(189, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Радиус маленького колеса ";
+            this.label2.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Анимация";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem рисоватьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem движениеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem пускToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem стопToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ 2D_Animation/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using Microsoft.DirectX;
+using Microsoft.DirectX.Direct3D;
+
+namespace _2D_Animation
+{
+
+    public partial class Form1 : Form
+    {
+        static public Point Center;
+        static public int d1 = 250, d2 = 100;
+        static public double omega = 0.5;
+        static public Pen p;
+        private double t = 0;
+        Graphics g = null;
+        bool fl = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(0, menuStrip1.Top + menuStrip1.Height);
+            pictureBox1.Width = this.ClientSize.Width;
+            pictureBox1.Height = this.ClientSize.Height;
+
+            Center.X = pictureBox1.Width / 5;
+            Center.Y = pictureBox1.Height / 10;
+            g = pictureBox1.CreateGraphics();
+        }
+
+        private void Picture(bool cl, Graphics g)
+        {
+            HatchBrush Br = new HatchBrush(HatchStyle.BackwardDiagonal, Color.Black, Color.White);
+            SolidBrush SBr = new SolidBrush(Color.White);
+            Pen p = new Pen(Color.Black);
+            g.Clear(Color.White);
+            g.FillEllipse(SBr, Center.X, Center.Y, d1, d1);
+            g.DrawEllipse(p, Center.X, Center.Y, d1, d1);
+            //рисование подшипника
+            p.DashStyle = DashStyle.Solid;
+            // линии подшипника
+            Point A = new Point(Center.X + d1 / 2, Center.Y + d1 / 2);
+            Point B = new Point(Center.X + d1 / 2 - 5, Center.Y + d1 / 2 + 10);
+            g.DrawLine(p, B, A);
+            B.X = Center.X + d1 / 2 + 5;
+            g.DrawLine(p, B, A);
+            //  подшипник
+            SBr.Color = Color.White;
+            g.FillEllipse(SBr, A.X - 2, A.Y - 2, 5, 5);
+            g.DrawEllipse(p, A.X - 2, A.Y - 2, 5, 5);
+            Br.Dispose();
+            // его линии
+            double f1 = t * omega;
+            p.DashStyle = DashStyle.DashDot;
+            Point l1 = new Point(Convert.ToInt32(Center.X + d1 / 2 - (d1 / 2) * Math.Sin(f1)),
+                Convert.ToInt32(Center.Y + d1 / 2 - (d1 / 2) * Math.Cos(f1)));
+            Point l2 = new Point(Convert.ToInt32(Center.X + d1/ 2 + (d1 / 2) * Math.Sin(f1)),
+                Convert.ToInt32(Center.Y + d1 / 2 + (d1 / 2) * Math.Cos(f1)));
+            g.DrawLine(p, l1, l2);
+            l1.X = Convert.ToInt32(Center.X + d1 / 2 - (d1 / 2) * Math.Cos(f1));
+            l1.Y = Convert.ToInt32(Center.Y + d1 / 2 + (d1 / 2) * Math.Sin(f1));
+            l2.X = Convert.ToInt32(Center.X + d1 / 2 + (d1 / 2) * Math.Cos(f1));
+            l2.Y = Convert.ToInt32(Center.Y + d1 / 2 - (d1 / 2) * Math.Sin(f1));
+            g.DrawLine(p, l1, l2);   
+            // второе колесо         
+            p.DashStyle = DashStyle.Solid;
+            Point O2 = new Point(Center.X + d1 - d2, Center.Y + d1 / 2 - d2 / 2);
+            Rectangle Rect = new Rectangle(O2.X, O2.Y, d2, d2);
+            //g.FillEllipse(SBr, Rect);
+            g.DrawEllipse(p, Rect);
+            // его линии 
+            p.DashStyle = DashStyle.DashDot;
+            double f2 = omega * d1 * t / d2;
+            Point t1 = new Point(Convert.ToInt32(O2.X + d2 / 2 - (d2 / 2) * Math.Sin(f2)), 
+                Convert.ToInt32(O2.Y + d2 / 2 - (d2 / 2) * Math.Cos(f2)));
+            Point t2 = new Point(Convert.ToInt32(O2.X + d2 / 2 + (d2 / 2) * Math.Sin(f2)), 
+                Convert.ToInt32(O2.Y + d2 / 2 + (d2 / 2)* Math.Cos(f2)));
+            g.DrawLine(p, t1, t2);
+            t1.X = Convert.ToInt32(O2.X + d2 / 2 - (d2 / 2) * Math.Cos(f2));
+            t1.Y = Convert.ToInt32(O2.Y + d2 / 2 + (d2 / 2) * Math.Sin(f2));
+            t2.X = Convert.ToInt32(O2.X + d2 / 2 + (d2 / 2) * Math.Cos(f2));
+            t2.Y = Convert.ToInt32(O2.Y + d2 / 2 - (d2 / 2) * Math.Sin(f2));
+            g.DrawLine(p, t1, t2);
+            // его подшипкник
+            p.DashStyle = DashStyle.Solid;
+            A.X = O2.X + d2 / 2;
+            A.Y = O2.Y + d2 / 2;
+            B.X = O2.X + d2 / 2 - 5;
+            B.Y = O2.Y + d2 / 2 + 10;
+            g.DrawLine(p, B, A);
+            B.X = O2.X + d2 / 2 + 5;
+            g.DrawLine(p, B, A);
+            SBr.Color = Color.White;
+            g.FillEllipse(SBr, A.X - 2, A.Y - 2, 5, 5);
+            g.DrawEllipse(p, A.X - 2, A.Y - 2, 5, 5);
+            // Линии грузов
+            p = new Pen(cl ? Color.Black : Color.White);
+            Point g1 = new Point(Center.X, Center.Y + d1 / 2);
+            Point g2 = new Point(Center.X, Center.Y + d1 + 10 + Convert.ToInt32(f1 * d1 / 2));
+            g.DrawLine(p, g1, g2);
+            g1.X = Center.X + d1 - d2;
+            g2.X = Center.X + d1 - d2;
+            g2.Y = Center.Y + d1 + 10 + Convert.ToInt32(f1 * d1 / 2);
+            g.DrawLine(p, g1, g2);
+            // Грузы
+            p = new Pen(cl ? Color.Black : Color.White);
+            Point k1 = new Point(Center.X - 15, Center.Y + d1 + 10 + Convert.ToInt32(f1 * d1 / 2));
+            Size s = new Size(30, 25);
+            Rectangle rekt1 = new Rectangle(k1, s);
+            g.DrawRectangle(p, rekt1);
+            k1.X = k1.X + d1 - d2;
+            k1.Y = Center.Y + d1 + 10 + Convert.ToInt32(f1 * d1 / 2);
+            Rectangle rekt2 = new Rectangle(k1, s);
+            if (k1.Y > Center.Y + d1 + 150)
+                fl = false;
+            if (k1.Y < Center.Y + d1)
+                fl = true;
+            g.DrawRectangle(p, rekt2);
+            p.Dispose();
+            SBr.Dispose();
+            this.Invalidate();
+        }
+
+        private void стопToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            рисоватьToolStripMenuItem.Enabled = true;
+        }
+
+        private void рисоватьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Picture(false, g);
+            t = 0;
+            Picture(true, g);
+            label1.Visible = true;
+            label2.Visible = true;
+            numericUpDown1.Visible = true;
+            numericUpDown2.Visible = true;
+            numericUpDown1.Enabled = true;
+            numericUpDown2.Enabled = true;
+            стопToolStripMenuItem.Enabled = true;
+            пускToolStripMenuItem.Enabled = true;
+            рисоватьToolStripMenuItem.Enabled = false;
+            fl = true;
+        }
+
+        private void пускToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Picture(false, g);
+            t += fl ? 0.1 : -0.1; 
+            Picture(true, g);
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            if (fl)
+                Picture(true, e.Graphics);
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            omega = Convert.ToDouble(numericUpDown1.Value) * 0.5;
+            
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            d2 = 20 * Convert.ToInt32(numericUpDown2.Value);
+            fl = false;
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            g.Dispose();
+            Close();
+        }
+    }
+}
